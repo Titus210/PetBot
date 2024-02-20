b797651300c74c1e4e662bf069f01108fb49c54d
Titus210 <tituskiplagat210@gmail.com>
2024-02-20 03:46:29 +0300

    feat: File Initial Setup
    - Installed All dependencies needed for the app

---
+++ frontend/PetBot/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+   <>
+   <h1 className="text-md text-green">Hello Tailwind</h1>
+   </>
+  )
+}
+
+export default App
---
+++ frontend/PetBot/tailwind.config.js
@@ -0,0 +1,46 @@
+import { Config } from 'tailwindcss';
+
+
+
+/** @type {import('tailwindcss').Config} */
+const config: Config ={
+  "content": [
+    "./src/**/*.{html,js,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'md': '1rem',  
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      }, colors: {
+        lightBlue: '#87CEEB',
+        blue: '#b44f43',
+
+        green: '#228B22',
+        brown: '#8B4513',         // Primary colors
+        
+        white: '#F5F5DC',        // white section
+        danger: '#FF2000',       // Red for danger
+        moderate: '#FFA500',     // Orange for moderate
+        safe: '#008000',         // Green for safe
+      },
+      fontFamily: {
+
+        'serif': ['Lora', 'serif'],
+        'sans': ['Montserrat', 'Quicksand', 'Roboto', 'sans'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+export default config
