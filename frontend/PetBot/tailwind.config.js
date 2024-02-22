c64d6aec63179e3849969d62206f628baf60782e
Titus210 <tituskiplagat210@gmail.com>
2024-02-22 03:17:54 +0300

    feat: Chaged Color Preference for the elements

--- frontend/PetBot/tailwind.config.js
+++ frontend/PetBot/tailwind.config.js
@@ -1,14 +1,25 @@
-import { Config } from 'tailwindcss';
-
-
-
-/** @type {import('tailwindcss').Config} */
-const config: Config ={
-  "content": [
-    "./src/**/*.{html,js,ts,tsx}",
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
   ],
   theme: {
     extend: {
+      colors: {
+        // Primary Colors
+        orange: "#F55713",  // Bright orange for highlights
+        red: "#D32F2F",     // Deep red for emphasis
+
+        // Neutral Colors
+        black: "#111",   // Black for strong contrasts
+        grey: "#808080",    // Medium grey for subtlety
+        white: "#ffffff",   // White for backgrounds
+
+        // Accent Colors
+        yellow: "#EBFF00",  // Yellow for warnings or notices
+        green: "#4CAF50",   // Green for success or positive elements
+        blue: "#004D99",   // Blue for buttons
+      },
       fontSize: {
         'xs': '.75rem',
         'sm': '.875rem',
@@ -20,27 +31,8 @@ const config: Config ={
         '4xl': '2.25rem',
         '5xl': '3rem',
         '6xl': '4rem',
-      }, colors: {
-        lightBlue: '#87CEEB',
-        blue: '#b44f43',
-
-        green: '#228B22',
-        brown: '#8B4513',         // Primary colors
-        
-        white: '#F5F5DC',        // white section
-        danger: '#FF2000',       // Red for danger
-        moderate: '#FFA500',     // Orange for moderate
-        safe: '#008000',         // Green for safe
-      },
-      fontFamily: {
-
-        'serif': ['Lora', 'serif'],
-        'sans': ['Montserrat', 'Quicksand', 'Roboto', 'sans'],
       },
     },
   },
   plugins: [],
-}
-
-
-export default config
+};
