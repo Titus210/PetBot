2fe2fb618f022d9825285d5beed5978bed39b843
Titus210 <tituskiplagat210@gmail.com>
2024-02-22 03:14:24 +0300

    feat:  PetBot Image / PetBot ImageCard - This is simple image card to iport in sections - Acts as logo

---
+++ frontend/PetBot/src/components/ui/cards/PetImageCard.tsx
@@ -0,0 +1,18 @@
+import chaBotImage from '../../../assets/images/bot-1.png'
+
+
+const PetImageCard = () => {
+    return (
+        <>
+            <div className="h-auto flex items-center rounded-full">
+                <img
+                    src={chaBotImage}
+                    alt="PetBot"
+                    className="h-48 w-48 mx-auto rounded-full"
+                />
+            </div>
+        </>
+    )
+}
+
+export default PetImageCard
