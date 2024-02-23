fe9a4254ccf19ab8d3fda144f0ab3819d0be8636
Titus210 <tituskiplagat210@gmail.com>
2024-02-23 06:00:48 +0300

    feat: Modified getstarted Page 1. Included Button Visibility - Composed Button TO modularized Structure

--- frontend/PetBot/src/pages/getStarted/GetStarted.tsx
+++ frontend/PetBot/src/pages/getStarted/GetStarted.tsx
@@ -1,41 +1,38 @@
-import { Link } from 'react-router-dom';
 import PetImageCard from '../../components/ui/cards/PetImageCard';
+import PrimaryButton from '../../components/ui/buttons/PrimaryButton';
 
 
 const GetStarted = () => {
   return (
     <>
-     <>
-      <div className="h-screen bg-white flex flex-col items-center justify-center md:w-7/12 mx-auto ">
-        <div className="flex flex-col items-center justify-between md:h-auto">
-          <div className=""></div>
+      <>
+        <div className="">
+          <div className="flex flex-col items-center justify-between md:h-auto">
+            <div className=""></div>
 
-          <div className="intro">
-            {/* Pet card */}
-            <PetImageCard />
+            <div className="intro">
+              {/* Pet card */}
+              <div className="">
+                <PetImageCard />
+              </div>
 
-            <h1 className="text-blue text-3xl font-bold text-center">
-              Welcome to <span className="yellow">PetBot </span>
-              <br />{" "}
-              <span className="text-md text-gray-500">
-                Your Personal Pet Assistant
-              </span>
-            </h1>
-          </div>
+              <div className="">
+                <h1 className="text-blue text-3xl font-bold text-center">
+                  Welcome to <span className="yellow">PetBot </span>
+                  <br />{" "}
+                  <span className="text-md text-gray-500">
+                    Your Personal Pet Assistant
+                  </span>
+                </h1>
+              </div>
+            </div>
 
-          <div className="button flex flex-col gap-3 w-full ">
-           
-            <div className="w-11/12 mx-auto text-center">
-              <Link to="/chat">
-                <p className="px-4 py-2 bg-blue text-white rounded-lg">
-                  Get Started
-                </p>
-              </Link>
+            <div className="button flex flex-col gap-3 w-full ">
+              <PrimaryButton link={"login"} colorVariation={"blue"} ButtonText='Get Started'/>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
     </>
   )
 }
