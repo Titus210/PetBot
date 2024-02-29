418527cd2fde7186f8b32b98a2b2429fa74fd136
Titus210 <tituskiplagat210@gmail.com>
2024-02-29 07:42:50 +0300

    feat: Style Setups

--- frontend/PetBot/src/App.tsx
+++ frontend/PetBot/src/App.tsx
@@ -5,9 +5,11 @@ function App() {
 
   return (
     <>
-      <UserRoutes />
+      <div className="h-screen bg-white flex flex-col items-center justify-center  mx-auto ">
+        <UserRoutes />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
