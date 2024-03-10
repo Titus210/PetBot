49ab238f60062b6c1912ee5f952082102c30b3c7
Titus210 <tituskiplagat210@gmail.com>
2024-03-10 14:21:15 +0300

    feat: Modularized code and removed uneccessary sections
    REAMINING : To change this into a siedebar

--- frontend/PetBot/src/pages/homePage/HomePage.tsx
+++ frontend/PetBot/src/pages/homePage/HomePage.tsx
@@ -2,38 +2,19 @@ import React from 'react'
 import PrimaryButton from '../../components/ui/buttons/PrimaryButton'
 import SecondaryButton from '../../components/ui/buttons/SecondaryButton'
 import Input from '../../components/ui/inputs/Input'
+import NavBar from '../../components/layouts/NavBar/NavBar'
+import { Footer } from 'flowbite-react'
 
 const HomePage = () => {
   return (
     <>
-      <div className="flex flex-col gap-4 justify-center items-center">
-        <div className="flex flex-col gap-4 text-center">
-          <h1 className="text-4xl font-bold">Welcome to PetBot</h1>
-        </div>
-        <div className="flex gap-2">
-          <div className="h-32 w-32 bg-slate-500  flex flex-col justify-center items-center rounded-full">
-            <p className="text-md">72% </p>
-            <p className="text-md">Performance </p>
-          </div>
-          <div className="h-32 w-32 bg-slate-500  flex flex-col justify-center items-center rounded-full">
-            <p className="text-md">72% </p>
-            <p className="text-md">Performance </p>
-          </div>
-          <div className="h-32 w-32 bg-slate-500  flex flex-col justify-center items-center rounded-full">
-            <p className="text-md">72% </p>
-            <p className="text-md">Performance </p>
-          </div>
-          <div className="h-32 w-32 bg-slate-500  flex flex-col justify-center items-center rounded-full">
-            <p className="text-md">72% </p>
-            <p className="text-md">Performance </p>
-          </div>
+      <div className="">
+        {/* NavBar */}
+        <NavBar/>
 
-        </div>
-        <div className="flex">
-          <div className="">
-            <SecondaryButton link={"chat"} colorVariation={"blue"} ButtonText="Start Training" />
-          </div>
-        </div>
+        <Main/>
+
+        <Footer/>
       </div>
     </>
   )
