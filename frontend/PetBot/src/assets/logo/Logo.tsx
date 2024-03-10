cb0a23cf6498dec4fe01bb0a176e0420fe167699
Titus210 <tituskiplagat210@gmail.com>
2024-03-10 14:38:05 +0300

    feat: Implementing Logo

---
+++ frontend/PetBot/src/assets/logo/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import  images_2 from "../../assets/images/bot-1.png"
+
+const Logo = () => {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+export default Logo
