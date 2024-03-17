5ef7b37dd9abf9d9f5f3130f657c3b3504971651
Titus210 <tituskiplagat210@gmail.com>
2024-03-17 08:08:59 +0300

    Refactor Chat component UI

--- frontend/PetBot/src/pages/chat/Chat.tsx
+++ frontend/PetBot/src/pages/chat/Chat.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import {Key, useState} from 'react';
 import Input from '../../components/ui/inputs/Input';
 
 interface UserPrompt {
   id: number;
   userPrompt: string;
   response: string;
+  prompt: string,
 }
 
-const initialUserPrompts: UserPrompt[] = [
- 
-];
+const initialUserPrompts: UserPrompt[] = [];
 
 const Chat: React.FC = () => {
   const [userPrompts, setUserPrompts] = useState<UserPrompt[]>(initialUserPrompts);
@@ -53,27 +52,39 @@ const Chat: React.FC = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col w-full relative">
-      <div className="mx-auto md:w-10/12">
-        <div className="flex-grow overflow-y-auto">
-          {userPrompts.map((prompt) => (
-            <div key={prompt.id} className="flex flex-col space-y-2 p-4">
-              <div className="flex items-center self-end w-12/12 md:max-w-2xl bg-slate-500 rounded-xl rounded-tr bg-blue-500 py-2 px-6 text-white">
-                <p className='  text-sm py-2'>{prompt.userPrompt}</p>
-              </div>
-              <div className="flex items-center self-start w-12/12 md:max-w-2xl rounded-xl rounded-tl bg-gray-300 py-2 px-6">
-                <p className='p-4 py-2 text-sm '>{prompt.response}</p>
-              </div>
+    <div className="flex h-screen flex-col py-3 bg-slate-400  items-center mx-auto  justify-between ">
+      {userPrompts.length === 0 && ( 
+        <div className="absolute top-[50%]">
+          <div className="self-center rounded-full">
+            {/* <Logo /> */}
+            <div className="">
+              <p>What Can I do for you today?</p>
             </div>
-          ))}
+          </div>
         </div>
+      )}
 
-        <Input
-          placeholder="Enter Your Prompt here"
-          value={inputValue}
-          onChange={handleInputChange}
-          onClick={handleSendClick}
-        />
+      <div className="overflow-y-auto w-11/12">
+        {userPrompts.map((prompt: { id: Key ; userPrompt: string ; response: string | number | boolean}) => (
+          <div key={prompt.id} className="flex flex-col space-y-2 p-4">
+            <div className="flex items-center self-end w-12/12 md:max-w-2xl bg-slate-500 rounded-xl rounded-tr bg-blue-500 py-2 px-6 text-white">
+              <p className='  text-sm py-2'>{prompt.userPrompt}</p>
+            </div>
+            <div className="flex items-center self-start w-12/12 md:max-w-2xl rounded-xl rounded-tl bg-gray-300 py-2 px-6">
+              <p className='p-4 py-2 text-sm '>{prompt.response}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="  right-0 w-11/12  ">
+        <div className="">
+          <Input
+            placeholder="Enter Your Prompt here"
+            value={inputValue}
+            onChange={handleInputChange}
+            onClick={handleSendClick}
+          />
+        </div>
       </div>
     </div>
   );
