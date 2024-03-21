e8573eab50fee2875416bd8a5117f72bb350d68a
Titus210 <tituskiplagat210@gmail.com>
2024-03-21 17:12:38 +0300

    feat: Input Styling

--- frontend/PetBot/src/components/ui/inputs/Input.tsx
+++ frontend/PetBot/src/components/ui/inputs/Input.tsx
@@ -6,14 +6,14 @@ interface InputProps {
 
 const Input: React.FC<InputProps> = ({ placeholder, value, onChange, onClick }) => {
     return (
-        <div className="flex items-center p-4">
+        <div className="flex items-center ">
             <input
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
-                className="w-11/12 p-2 border-2 border-gray-200 rounded-lg"
+                className="w-11/12 p-2 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
             />
-            <button onClick={onClick} className="bg-blue text-white text-xl px-6 py-2">Send</button>
+            <button onClick={onClick} className="bg-blue text-white text-xl px-6 py-2 rounded-lg ">Send</button>
         </div>
     );
 };
