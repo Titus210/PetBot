c9d447aab05662cb7711a11853d5702eb6328bc4
Titus210 <tituskiplagat210@gmail.com>
2024-03-21 17:13:21 +0300

    feat: Created User Styles

--- frontend/PetBot/src/routes/UserRoutes.tsx
+++ frontend/PetBot/src/routes/UserRoutes.tsx
@@ -8,23 +8,35 @@ import Login from "../pages/login/Login";
 import SignUp from "../pages/signup/SignUp";
 import SelectPet from "../pages/signup/SelectPet";
 import SelectBreed from "../pages/signup/SelectBreed";
-import {PetAge, PetName} from "../pages/signup/NameSelection";
+import { PetAge, PetName } from "../pages/signup/NameSelection";
+import Logout from "../pages/logout/Logout";
+import HelpSupport from "../pages/helpSupport/HelpSupport";
+import Settings from "../pages/account/Account";
 
 const UserRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<GetStarted />} />
-                <Route path="/homepage" element={<HomePage />} />
+
+                <Route path="/homepage/" element={<HomePage />} >
+                    <Route path="/homepage/chat" element={<Chat />} />
+                    <Route path="/homepage/logout" element={<Logout />} />
+                    <Route path="/homepage/account-information" element={<Settings />} />
+                    <Route path="/homepage/help-support" element={<HelpSupport />} />
+                </Route>
+
+
                 <Route path="/chat" element={<Chat />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/pet-selection" element={<SelectPet />} />
                 <Route path="/breed-selection" element={<SelectBreed />} />
-                
+
                 <Route path="/pet-name" element={<PetName />} />
                 <Route path="/pet-age" element={<PetAge />} />
-                
+    
+                <Route path="help-support" element={< HelpSupport />} />
                 <Route element={<NotFound />} />
             </Routes>
         </Router>
@@ -32,3 +44,51 @@ const UserRoutes = () => {
 };
 
 export default UserRoutes;
+
+/*
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GetStarted from "../pages/getStarted/GetStarted";
+import HomePage from "../pages/homePage/HomePage";
+import Chat from "../pages/chat/Chat";
+import NotFound from "../pages/notFound/NotFound";
+import Login from "../pages/login/Login";
+import SignUp from "../pages/signup/SignUp";
+import SelectPet from "../pages/signup/SelectPet";
+import SelectBreed from "../pages/signup/SelectBreed";
+import { PetAge, PetName } from "../pages/signup/NameSelection";
+import AccountInfo from "../pages/account/Account";
+import HelpSupport from '../pages/helpSupport/HelpSupport';
+
+const UserRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+
+                <Route path="/" element={<GetStarted />} />
+
+                <Route path="/homepage/" element={<HomePage />} >
+                    <Route path="/chat" element={<Chat />} />
+                    <Route path="/logout"  element = {<Logout/>}/>
+                    <Route path="/account-information" element={<AccountInfo />}/>
+                </Route>
+
+                <Route path="/login" element={<Login />} />
+
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/pet-selection" element={<SelectPet />} />
+                <Route path="/breed-selection" element={<SelectBreed />} />
+                <Route path="/pet-name" element={<PetName />} />
+                <Route path="/pet-age" element={<PetAge />} />
+
+                
+
+                <Route path ="/*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default UserRoutes;
+
+*/
